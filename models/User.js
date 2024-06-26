f8630f833966bca5c392402a0fe8e784320538d6
MOH2056 <tapereyetunde@gmail.com>
2024-06-26 20:20:19 +0100

    saved file

--- models/User.js
+++ models/User.js
@@ -14,4 +14,17 @@ const loginSchema = new mongoose.Schema({
         required: true,
         unique: true,
     }
-})
+});
+
+const User = mongoose.model('User', loginSchema);
+
+User.findOne({ Email: email }, (err, user) => {
+    if (!user) {
+        res.status(401).send('Email not found');
+    } else {if (password === user.password) {
+        res.send('Login successful');
+    } else {
+        res.status(401).send('Incorrect password');
+    }
+}
+});
