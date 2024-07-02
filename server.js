d033c947d9c6bed39fd7ad3cc8d2e8fb98d234aa
hadi387 <157270301+hadi387@users.noreply.github.com>
2024-07-02 19:29:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+
 const server = express();
-const userRoutes = require('./routes/userroutes');
 const PORT = 5000;
+
 server.use(express.json());
 
-server.use('/api/users', userroutes)
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/expressAuthRBAC', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
 
-mongoose.connect('mongodb://localhost:27017/expressAuthRBAC')
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+// Use routes
+server.use('/api/users', userRoutes);
 
 server.listen(PORT, () => {
-    console.log("SERVER IS WORKING");
-})
+    console.log(`SERVER IS WORKING`);
+});
